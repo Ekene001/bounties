c85afb6367e93291929b2d6e5193d21a66375c78
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2026-01-25 21:47:01 +0100

    state management hooks added

---
+++ hooks/use-throttle.ts
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+/**
+ * Hook to throttle a value.
+ * 
+ * @param value The value to throttle.
+ * @param limit The throttle limit in milliseconds.
+ * @returns The throttled value.
+ * 
+ * @example
+ * const throttledValue = useThrottle(value, 1000);
+ */
+export function useThrottle<T>(value: T, limit: number): T {
+    const [throttledValue, setThrottledValue] = React.useState<T>(value)
+    const lastRan = React.useRef<number>(Date.now())
+
+    React.useEffect(() => {
+        const handler = setTimeout(
+            () => {
+                if (Date.now() - lastRan.current >= limit) {
+                    setThrottledValue(value)
+                    lastRan.current = Date.now()
+                }
+            },
+            limit - (Date.now() - lastRan.current)
+        )
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, limit])
+
+    return throttledValue
+}
